078a2e0f8c1b5c2472213d2897caa55ba28cea18
Melanie Harris <mell.j.harris@gmail.com>
2019-06-04 15:46:31 -0700

    test

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -4,7 +4,7 @@ import './Display.css';
 const CalculatorDisplay= props =>{
     return(
         <div className="calculatorDisplay">
-            <p className="result">0</p>
+            <p className="thisDisplays">0</p>
         </div>
     );
 };
