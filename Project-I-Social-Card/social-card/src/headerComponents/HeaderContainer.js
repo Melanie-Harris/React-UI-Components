8105460bdad88b332018516e51acc925963a43fb
Melanie Harris <mell.j.harris@gmail.com>
2019-06-04 15:47:35 -0700

    Social-card commit

---
+++ Project-I-Social-Card/social-card/src/headerComponents/HeaderContainer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import HeaderTitle from './HeaderTitle';
+// import HeaderContent from '.HeaderContent';
+// import ImageThumbnail from '.ImageThumbnail';
+
+const HeaderContainer = ()=>{
+    return(
+        <div className='headerContainer'>
+            
+                <HeaderTitle />
+                {/* <HeaderContent />
+                <ImageThumbnail /> */}
+            
+        </div>
+    );
+};
+
+export default HeaderContainer;
