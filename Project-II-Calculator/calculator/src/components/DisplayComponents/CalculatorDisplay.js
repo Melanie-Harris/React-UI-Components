3066970669486c25463376ac536509cbfa4b9131
Melanie Harris <mell.j.harris@gmail.com>
2019-06-11 15:35:20 -0700

    updates made

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -5,6 +5,7 @@ const CalculatorDisplay= props =>{
     return(
         <div className="calculatorDisplay">
             <p className="thisDisplays">0</p>
+            {/* <input type="text" className="thisDisplays">0</input> */}
         </div>
     ); 
 };
